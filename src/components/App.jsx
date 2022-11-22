17dbdab8c70591ac0c4651acceee5ca571c5b8ea
stefaniacamargo <estefaniacamargo263@gmail.com>
2022-11-22 11:04:34 -0500

    configuracion bootstrap y estilos de la vista Login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,49 @@
-import { useState } from 'react';
-import thumb from '../../thumb.png';
+import { useState } from "react";
+import urbanBurger from "../../public/Urban Burger (6).png";
 
 const App = () => {
-  const [times, setTimes] = useState(0);
-
-  const onButtonClick = () => {
-    setTimes(value => value + 1)
-  };
-
   return (
-    <>
-      <h1>Hello World</h1>
-      <img src={thumb} alt="Thumb image" />
-      <button onClick={onButtonClick}>Click me! {times}</button>
-    </>
+    <div class="login-bg">
+      <main class="container login-container">
+        <div class="login-form w-100 m-auto d-flex flex-column">
+          <img
+            class="login-image m-auto"
+            src={urbanBurger}
+            alt="Login Image"
+          />
+          <form class="p-4">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <div class="form-floating">
+              <input
+                type="email"
+                class="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="password"
+                class="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Remember me
+              </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -35,12 +35,23 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
         ],
       },
     ],
